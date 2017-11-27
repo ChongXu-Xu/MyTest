71c07319f0a5be0ae9a3d864eb4f81dd6b244fb7
ChongXu-Xu <499837687@qq.com>
2017-11-27 18:30:23 +0800

    Change-Id: I0000000000000000000000000000000000000002 Signed-off-by: ChongXu-Xu <499837687@qq.com>

--- src/scott/Person.java
+++ src/scott/Person.java
@@ -1,5 +1,14 @@
 package scott;
 
 public class Person {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
