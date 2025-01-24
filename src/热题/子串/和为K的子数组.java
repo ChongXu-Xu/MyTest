f7db1a4cf8d67dafdaad5b98f74d3c42e851b30a
许崇栩hz21066763 <hz21066763>
2025-01-24 17:54:30 +0800

    2025-01-24

--- src/热题/子串/和为K的子数组.java
+++ src/热题/子串/和为K的子数组.java
@@ -1,5 +1,7 @@
 package 热题.子串;
 
+import java.util.HashMap;
+
 public class 和为K的子数组 {
 
     // 难度：中等
@@ -29,6 +31,23 @@ public class 和为K的子数组 {
                     count++;
                 }
             }
+        }
+        return count;
+    }
+
+    // 方法二：前缀和 + 哈希
+    // 假设pre[i]为[0....i]的总和，那[j...i]的总和为pre[i]-pre[j-1]，即pre[i]-pre[j-1]==k
+    // 即求i前面，有j-1符合这个pre[i]-pre[j-1]==k的pre[j-1]有多少个
+    public static int subarraySum2(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            if (mp.containsKey(pre - k)) {
+                count += mp.get(pre - k);
+            }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
         }
         return count;
     }
